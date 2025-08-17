5cd6053776dd5b0856baa0c213667d3d12ef4c78
Mazuru Gabriel <gabrielmazuru6@gmail.com>
2025-08-17 15:39:46 +0300

    adding password to a text file

--- managePassword.py
+++ managePassword.py
@@ -4,16 +4,28 @@ secretWord = "maya"
 appPasswordVerify = input("What is the app pasword? : ")
 secretWordVerify = input("What is the secret word? : ")
 
+def startApp():
+    print("Welcome to your password manager python app!")
+    mode = input("What would you like to do, add password or see a existing password? press q to quit : ").lower()
+    if mode == "q":
+        print("Goodbye!")
+    elif mode == "add":
+        accountName = input("Enter the name of the account : ")
+        accountPassword = input("Enter the password of the account : ")
+
+        with open('password.txt', 'a') as f:
+            f.write(accountName + " | " + accountPassword) 
+
 def tryAgainFunction():
     appPasswordVerify = input("What is the app pasword? : ")
     secretWordVerify = input("What is the secret word? : ")
     if appPasswordVerify == "1234" and secretWordVerify == "maya":
-        pass
+        startApp()
     else:
         print("Try again later")
 
 if appPasswordVerify == "1234" and secretWordVerify == "maya":
-    pass
+    startApp()
 
 else:
     tryAgainQuestion = input("Do you want to try again?(Y/N) : ").lower()
